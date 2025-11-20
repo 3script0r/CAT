e1f2711ae8a2ce10447fe26a86fc17ab51834af6
Eduardo Fuentes <eduardogermanfuentescorzo@gmail.com>
2025-11-20 10:58:40 -0500

    uno

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'https://3script0r.github.io/CAT',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base:'https://3script0r.github.io/CAT/',
+ 
 });
