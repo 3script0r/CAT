b09ed3e0d512075ca111f5ab6ffe204d20881b71
3script0r <eduardogermanfuentescorzo@gmail.com>
2025-11-20 19:00:40 -0500

    Actualizar vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://3script0r.github.io/CAT',
+  base:'./',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
